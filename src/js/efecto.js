91d9e3e7957b45778850dfbb54ff1d90e48e611d
kidjavi3 <4b3javi@gmail.com>
2018-01-23 02:39:07 +0100

    version1

---
+++ src/js/efecto.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('.slideshow').cycle({
+        fx: 'fade',
+        sync: false,
+        delay: -2000// choose your transition type, ex: fade, scrollUp, shuffle, etc...
+    });
+});
